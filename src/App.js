35ee5eaaa97506146c8a343b3112a72281807a40
Madhurjya Pegu <migom@Madhurjyas-MacBook-Air.local>
2017-09-24 23:19:05 +0530

    charts

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
     super(props);
     this.state = {
       loading: false, 
-      auth: false
+      auth: true
     }
   }
   componentDidMount() {
